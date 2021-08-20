4e745c71fe3dda77d3c4da42d2be8e9d491d1ba2
Mike Mallon <mallon127@gmail.com>
2021-08-20 18:37:31 -0500

    Added user input and assigned user input variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,81 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Array of special characters 
+var speicalCharacters = ["!,#$%&'()*+,-./:;<=>?@[\]^_`{|}~ "];
+var numbers = [0,1,2,3,4,5,6,7,8,9];
+var upperCaseLetters = ["A","B","CDEFGHIJKLMNOPQRSTUVWXYZ"];
+var lowerCaseLetters = ["abcdefghijklmnopqrstuvwxyz"]
+
+// User input variables
+var passwordLength = 0;
+var useUpperCase = true;
+var useLowerCase = true;
+var useNumbers = true;
+var useSpecialCharacters = true;
+
+var password = [];
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+    
+  var passwordLength = window.prompt("How long would you like your password to be? (8 - 128)");
+  var useUpperCase = window.confirm("Do you want your password to contain upper case letters?");
+  var useLowerCase = window.confirm("Do you want your password to contain lower case letters?");
+  var useNumbers = window.confirm("Do you want your password to contain numbers?");
+  var useSpecialCharacters = window.confirm("Do you want your password to contain special characters?")
+  
+  var characterCounter = 0;
+
+  for(let i = 0; i < passwordLength; i++){
+    console.log(i);
+
+    let charactersToChooseFrom = [];
+
+    if(useUpperCase == true){
+  
+      charactersToChooseFrom.push(upperCaseLetters);
+      characterCounter = characterCounter + upperCaseLetters.length;
+    }
+    if(useLowerCase == true){
+      charactersToChooseFrom.push(lowerCaseLetters);
+      characterCounter = characterCounter + lowerCaseLetters.length;
+    }
+    if(useNumbers == true){
+      charactersToChooseFrom.push(numbers);
+      characterCounter = characterCounter + numbers.length;
+    }
+    if(useSpecialCharacters == true){
+      charactersToChooseFrom.push(speicalCharacters);
+      characterCounter = characterCounter + speicalCharacters.length;
+    }
+
+    
+
+    console.log(charactersToChooseFrom);
+    console.log(charactersToChooseFrom.length)
+    console.log(characterCounter)
+
+  }
+
+
+  //var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+
+  //Writes the new password to the HTML front-end
+  //passwordText.value = password;
+  passwordText.value = passwordLength;
 
 }
 
-// Add event listener to generate button
+// Triggeres writePassword function
 generateBtn.addEventListener("click", writePassword);
+
+
+
+//THEN my input should be validated and at least one character type should be selected
+//WHEN all prompts are answered
+//THEN a password is generated that matches the selected criteria
+//WHEN the password is generated
+//THEN the password is either displayed in an alert or written to the page
---
+++ assets/js/testScripts.js
@@ -0,0 +1,7 @@
+var passwordLength = "";
+
+var testCode = function (){
+    var passwordLength = window.prompt("How many characters would you like your password to be? (Minimum: 8)");
+  }
+  
+  testCode();
