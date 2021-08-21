9c2986cc0f8a4f099b4483a26777d42f36ac8004
Mike Mallon <mallon127@gmail.com>
2021-08-20 23:23:18 -0500

    Added validation to the password lenght

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,27 +9,26 @@ var numbers = ['0','1','2','3','4','5','6','7','8','9'];
 var upperCaseLetters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var lowerCaseLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-// User input variables
-var passwordLength = 0;
-var useUpperCase = true;
-var useLowerCase = true;
-var useNumbers = true;
-var useSpecialCharacters = true;
 
 var password = [];
 
 
-function requestUserInput(){
-  
+function getPasswordLength(){
 
 
+  return;
 }
 
 
 // Write password to the #password input
 function writePassword() {
     
-  var passwordLength = window.prompt("How long would you like your password to be? (8 - 128)");
+  var passwordLength = 0;
+
+  while(passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)==true){
+    var passwordLength = window.prompt("How long would you like your password to be? (8 - 128)");
+  }
+
   var useUpperCase = window.confirm("Do you want your password to contain upper case letters?");
   var useLowerCase = window.confirm("Do you want your password to contain lower case letters?");
   var useNumbers = window.confirm("Do you want your password to contain numbers?");
@@ -89,9 +88,3 @@ var charactersToChooseFrom = [];
 generateBtn.addEventListener("click", writePassword);
 
 
-
-//THEN my input should be validated and at least one character type should be selected
-//WHEN all prompts are answered
-//THEN a password is generated that matches the selected criteria
-//WHEN the password is generated
-//THEN the password is either displayed in an alert or written to the page
